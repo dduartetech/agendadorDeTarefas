c1a7608159f683a7eeb892d5a160b8a58e5e971b
Diego <diegocavalcante040307@gmail.com>
2025-08-31 14:45:36 -0300

    commit inicial

---
+++ src/test/java/com/diegoduarte/agendadorDeTarefas/AgendadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoduarte.agendadorDeTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
