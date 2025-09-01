e0ea7d9e65c8c09cd4420fce57794487db293f20
Diego <diegocavalcante040307@gmail.com>
2025-09-01 17:08:20 -0300

    correção de apontamentos

--- src/main/java/com/diegoduarte/agendadorDeTarefas/infrascture/security/UserDetailsServiceImpl.java
+++ src/main/java/com/diegoduarte/agendadorDeTarefas/infrascture/security/UserDetailsServiceImpl.java
@@ -2,13 +2,9 @@ package com.diegoduarte.agendadorDeTarefas.infrascture.security;
 
 import com.diegoduarte.agendadorDeTarefas.business.dto.UsuarioDTO;
 import com.diegoduarte.agendadorDeTarefas.infrascture.client.UsuarioClient;
-import com.diegoduarte.cadastroDeUsuario.infrastructure.entity.Usuario;
-import com.diegoduarte.cadastroDeUsuario.infrastructure.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
