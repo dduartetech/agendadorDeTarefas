f80754a908332354934e1c9f8a60255d907be5b6
Diego Duarte <diegocavalcante040307@gmail.com>
2025-09-14 19:13:55 -0300

    apontamento de ajustes

--- src/main/java/com/diegoduarte/agendadorDeTarefas/business/dto/UsuarioDTO.java
+++ src/main/java/com/diegoduarte/agendadorDeTarefas/business/dto/UsuarioDTO.java
@@ -2,15 +2,12 @@ package com.diegoduarte.agendadorDeTarefas.business.dto;
 
 import lombok.*;
 
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UsuarioDTO {
-
     private String email;
     private String senha;
 
--- src/main/java/com/diegoduarte/agendadorDeTarefas/infrascture/client/UsuarioClient.java
+++ src/main/java/com/diegoduarte/agendadorDeTarefas/infrascture/client/UsuarioClient.java
@@ -1,13 +1,13 @@
 package com.diegoduarte.agendadorDeTarefas.infrascture.client;
 
-import com.diegoduarte.agendadorDeTarefas.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "usuario", url = "{$usuario.url}")
-public interface UsuarioClient {
+@FeignClient(name = "usuario", url = "${usuario.url}")
+public class UsuarioClient {
 
     @GetMapping
     UsuarioDTO buscaPorEmail(@RequestParam("email")  String email,
