5d41d9c9cdde2783f7af00901ee142419830a8e6
Diego Duarte <diegocavalcante040307@gmail.com>
2025-09-14 19:41:20 -0300

    apontamento de ajustes

--- src/main/java/com/diegoduarte/agendadorDeTarefas/infrascture/client/UsuarioClient.java
+++ src/main/java/com/diegoduarte/agendadorDeTarefas/infrascture/client/UsuarioClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "usuario", url = "${usuario.url}")
-public class UsuarioClient {
+public interface UsuarioClient {
 
     @GetMapping("/usuario")
     UsuarioDTO buscaPorEmail(@RequestParam("email")  String email,
